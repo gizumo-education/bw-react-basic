3440fd9ac1a2f327bab47b4ac5688f63eec0b406
Ryo Kihara <kihara@gizumo-inc.jp>
2023-07-07 08:59:21 +0900

    complete.

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
 export const TodoItem = ({ name, isToday, isCompoleted }) => {
-  return <li className={styles.name}>{name}</li>
+  return isToday && <li className={styles.name}>{isCompoleted ? name + '✔︎' : name}</li>
 }
 
 TodoItem.propTypes = {
