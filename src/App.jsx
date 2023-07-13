698dd33634977afa711e669143c5d66cb92141d2
“rinoi3o” <kutianfeiniao@gmail.com>
2023-07-13 12:13:40 +0900

    条件付きレンダー

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Button } from './components/Button'
 import { Profile } from './components/Profile/index.jsx'
 import { Country } from './components/Country/index.jsx';
 import { Card } from './components/Card/index.jsx';
+import { TodoItem } from './components/TodoItem/index.jsx';
+
 
 export const App = () => {
   return (
@@ -13,9 +15,9 @@ export const App = () => {
 
       {/* 以下に練習問題の回答を記述してください */}
       <hr />
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
       <hr />
       {/* Profileコンポーネントのタグをここに記述 */}
       <Profile></Profile>
@@ -30,7 +32,12 @@ export const App = () => {
         height={80} />
       </Card>
       <hr />
-      {/* CardコンポーネントとCountryコンポーネントのタグをここに記述 */}
+      {/* ulタグとTodoItemコンポーネントのタグをここに記述 */}
+        <ul>
+          <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+          <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+          <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+        </ul>
     </div>
   )
 }
--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,12 +1,41 @@
+// import PropTypes from 'prop-types'
+// import styles from './index.module.css'
+
+// export const TodoItem = ({ name, isToday, isCompleted }) => {
+//   return <li className={styles.name}>{name}</li>
+// }
+
+// TodoItem.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   isToday: PropTypes.bool.isRequired,
+//   isCompleted: PropTypes.bool.isRequired,
+// }
 import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
-export const TodoItem = ({ name, isToday, isCompoleted }) => {
-  return <li className={styles.name}>{name}</li>
-}
+export const TodoItem = ({ name, isToday, isCompleted }) => {
+  if (isToday) {
+    if (isCompleted) {
+      return (
+        <li className={styles.name}>
+          {name} <span >✔︎</span>
+        </li>
+      );
+    } else {
+      return <li className={styles.name}>{name}</li>;
+    }
+  } else {
+    return null;
+  }
+};
 
 TodoItem.propTypes = {
   name: PropTypes.string.isRequired,
   isToday: PropTypes.bool.isRequired,
-  isCompoleted: PropTypes.bool.isRequired,
+  isCompleted: PropTypes.bool.isRequired,
 }
+
+
+
+// {isToday ? <className /> : <TodoItem />}
+// {isCompleted ? <TodoItem /> : <name/>}
