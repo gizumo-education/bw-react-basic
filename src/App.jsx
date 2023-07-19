1e28ca96144ee798afa038a7444ec92d05c9dbad
iriguthi <iriguthi.ryoma@gmail.com>
2023-07-19 10:34:44 +0900

    sec6props

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import { Button } from './components/Button'
 import { Profile } from './components/Profile/index.jsx'
+import { Country } from './components/Country'
+import { Card } from './components/Card'
 
 export const App = () => {
   return (
     <div>
     <img src={reactLogo} className={styles['react-logo']} />
     <h1>Hello React!</h1>
-
     {/* 以下に練習問題の回答を記述してください */}
     <hr />
     <Button></Button>
@@ -16,8 +17,19 @@ export const App = () => {
     <Button></Button>
 
     <hr />
-    {/* Profileコンポーネントのタグをここに記述  */}
+    {/* Profileコンポーネントのタグをここに記述*/}
     <Profile></Profile>
+
+    <hr />
+    {/*CardコンポーネントとCountryコンポーネントのタグをここに記述*/}
+    <Card>
+      <Country info={{
+        imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+        name: 'アメリカ'
+      }}
+      width={120}
+      height={80}/>
+    </Card>
     </div>
   )
 }
