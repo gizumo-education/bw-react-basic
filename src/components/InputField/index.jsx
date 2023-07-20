8f1fbaa9913c0b8936393aeaec889829ac31af50
iriguthi <iriguthi.ryoma@gmail.com>
2023-07-20 16:23:28 +0900

    sec10フックとuseState

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,12 +1,28 @@
 import styles from './index.module.css'
+import { useState } from 'react'
 
-export const InputField = () => {
+/*export const InputField = () => {
   let value = ''
 
   return (
     <>
       <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
       <p>入力文字：{value}</p>
+    </>
+  )
+}
+*/
+
+export const InputField = () => {
+  const [value, setvalue] = useState('')
+  const handleInput = (e) => {
+    setvalue(e.target.value)
+  }
+
+  return (
+    <>
+    <input onChange={handleInput} className={styles.input} placeholder='任意の文字を入力' />
+    <p>入力文字：{value}</p>
     </>
   )
 }
