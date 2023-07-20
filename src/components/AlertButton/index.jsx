b5348b89fe00e43b1afc0965bf55b27ae8f3406e
iriguthi <iriguthi.ryoma@gmail.com>
2023-07-20 12:52:47 +0900

    sec9イベントハンドラ

--- src/components/AlertButton/index.jsx
+++ src/components/AlertButton/index.jsx
@@ -1,5 +1,13 @@
 import styles from './index.module.css'
 
-export const AlertButton = () => {
+/*export const AlertButton = () => {
   return <button className={styles.button}>Alert</button>
+}
+*/
+
+export const AlertButton =() => {
+  const handleAlert = () => {
+    alert('暑い、、、')
+  }
+  return <button className={styles.button} onClick={handleAlert}>Alert</button>
 }
