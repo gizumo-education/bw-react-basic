3cfcfc7120e892d87f62ac41b4f37e3a3a66da6e
中村優穂 <yuho@nakamurayuuhonoMacBook-Pro-257.local>
2023-07-25 13:31:41 +0900

    feat:section7

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Button } from './components/Button/index'
 import { Profile } from './components/Profile/index'
 import { Country } from './components/Country/index'
 import { Card } from './components/Card/index'
+import { TodoItem } from './components/TodoItem/index'
 
 export const App = () => {
   return (
@@ -28,6 +29,11 @@ export const App = () => {
         />
       </Card>
       <hr />
+      <ul>
+        <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+        <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+        <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+      </ul>
 
 
     </div>
