6480a752cead21b162045a183cd837c193596892
中村優穂 <yuho@nakamurayuuhonoMacBook-Pro-257.local>
2023-07-25 16:31:38 +0900

    feat:section10

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Card } from './components/Card/index'
 import { TodoItem } from './components/TodoItem/index'
 import { LanguageList } from './components/LanguageList/index'
 import { AlertButton } from './components/AlertButton/index'
+import { InputField } from './components/InputField/index'
 
 export const App = () => {
   return (
@@ -40,6 +41,8 @@ export const App = () => {
       <LanguageList />
       <hr />
       <AlertButton />
+      <hr />
+      <InputField />
     </div>
 
   )
--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,11 +1,25 @@
 import styles from './index.module.css'
+import { useState } from 'react'
 
 export const InputField = () => {
-  let value = ''
+  // let value = useState('')
+  //   return (
+  //     <>
+  //       <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
+  //       <p>入力文字：{value}</p>
+  //     </>
+  //   )
+  // }
 
+
+  // let value = useState('')
+  let [value, setInputValue] = useState('');
+  const handleChange = (event) => {
+    setInputValue(event.target.value); // インプットエリアの内容を状態に反映
+  };
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
+      <input onChange={handleChange} className={styles.input} placeholder='任意の文字を入力' />
       <p>入力文字：{value}</p>
     </>
   )
