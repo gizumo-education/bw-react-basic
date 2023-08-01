ab76c50b1a2721b2d5a901eaa72626e39b4deec0
izumikatsuko <gsnhj522step@gmail.com>
2023-08-01 12:10:50 +0900

    JSX構文＿プロフィールの表示

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,36 @@ import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 <hr />
 import { Button } from './components/Button/index';
+import { Profile } from './components/Profile/index';
 
 // ※ src={reactLogo} の書き方はJSX構文の書き方 後ほど...
 // ※ className={styles['react-logo']} の記述はHTMLのclassに当たる書き方 後ほど...
 export const App = () => {
   return (
-   <div>
+    <div>
       <img src={reactLogo} className={styles['react-logo']} />
       <h1>Hello React!</h1>
 
-<hr />{/* 区切り線 */}
+      <hr />{/* 区切り線 */}
 
-<div>
-  <Button />
-  <Button />
-  <Button />
-  
-</div>
+      <div>
+        <Button />
+        <Button />
+        <Button />
 
-<hr />{/* 区切り線 */}
+      </div>
+
+      <hr />{/* 区切り線 */}
+        <>
+          <Profile />
+        </>
+      <hr />{/* 区切り線 */}
 
 
-      
-      
-      
-   </div>
-   
+
+
+
+    </div>
+
   )
 }
