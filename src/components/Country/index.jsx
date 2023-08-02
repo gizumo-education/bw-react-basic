4017a0f2dfec0b1673f017787cb3360292b0423c
izumikatsuko <gsnhj522step@gmail.com>
2023-08-02 14:50:54 +0900

    Props問題1と問題2

--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,10 +1,48 @@
 import styles from './index.module.css'
 
-export const Country = () => {
+
+
+// > prop-typesライブラリのインポート
+import PropTypes from 'prop-types'
+
+// Logo コンポーネント(子)  ＜受け取り側＞
+export const Country = ({ Info, name }) => {
   return (
-    <div>
-      <img src={info.imgUrl} width={width} height={height} />
-      <p className={styles.name}>{info.name}</p>
-    </div>
+    <>
+      <img
+        src={ Info.imgUrl }
+      />
+      <p className={styles.name}>{ Info.name}</p>
+    </>
   )
 }
+
+// 型を明示的にする記述を追加
+// > 1_prop-typesライブラリのインポート→ 17行目
+
+// > 2_コンポーネント名.propTypesにpropsの型を定義する
+Country.protoTypes = {
+  Info: PropTypes.exact({
+    url: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+  }),
+  width: PropTypes.number,
+  height: PropTypes.number
+}
+
+
+// デフォルト値
+Country.defaultProps = {
+  width: 120,
+  height: 80
+}
+
+// 元
+// export const Country = () => {
+//   return (
+//     <div>
+//       <img src={info.imgUrl} width={width} height={height} />
+//       <p className={styles.name}>{info.name}</p>
+//     </div>
+//   )
+// }
