f879619274f3decbe425fd5473bb161f971cb9d0
izumikatsuko <gsnhj522step@gmail.com>
2023-08-02 14:39:57 +0900

    props 問1と問2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import styles from './App.module.css'
 <hr />
 import { Button } from './components/Button/index';
 import { Profile } from './components/Profile/index';
-
+import { Country } from './components/Country/index';
+import { Card } from './components/Card/index';
+import { TodoItem } from './components/TodoItem/index';
 // ※ src={reactLogo} の書き方はJSX構文の書き方 後ほど...
 // ※ className={styles['react-logo']} の記述はHTMLのclassに当たる書き方 後ほど...
 export const App = () => {
@@ -22,14 +24,47 @@ export const App = () => {
       </div>
 
       <hr />{/* 区切り線 */}
-        <>
-          <Profile />
-        </>
+      <>
+        <Profile />
+      </>
+
+      <hr />{/* 区切り線 */}
+      <>
+        {/* Country(子)へ渡す */}
+        <Country
+          Info={{
+            imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+            name: 'アメリカ'
+          }}
+        />
+
+      </>
+
       <hr />{/* 区切り線 */}
 
+      <>
+        {/* Childrenを渡す */}
 
+        <Card>
+          <Country
+            Info={{
+              imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+              name: 'アメリカ'
+            }}
+          />
+        </Card>
+      </>
 
+      <hr />{/* 区切り線 */}
+      <>
+        <TodoItem />
+        <ul>
 
+          <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+          <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+          <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+        </ul>
+      </>
 
     </div>
 
