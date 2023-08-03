3514040af76d1cc6969858ebf7f7b8e91278a9fa
izumikatsuko <gsnhj522step@gmail.com>
2023-08-03 17:45:52 +0900

    イベントハンドラ問題1

--- src/components/AlertButton/index.jsx
+++ src/components/AlertButton/index.jsx
@@ -1,5 +1,25 @@
 import styles from './index.module.css'
 
 export const AlertButton = () => {
-  return <button className={styles.button}>Alert</button>
+  
+  const handleAlert = () => {
+    alert('削除します。よろしいですか？')
+  }
+  return <button className={styles.button} onClick={handleAlert}>Alert</button>
 }
+
+
+
+// 削除します。よろしいですか？
+
+// 問題
+// 「Alert」ボタンをクリックしたらアラートが表示されるようにすること
+// アラートに表示させる文字は任意の文字でOKです。
+// src/App.jsx にAlertButtonコンポーネントをインポートして、JSX内に追記し、画面に表示できるようにすること
+
+//元
+// export const AlertButton = () => {
+//   return <button className={styles.button}>Alert</button>
+// }
+
+
