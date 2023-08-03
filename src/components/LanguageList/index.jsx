99afdccc49c96e78f69f7be86c6457eac752dc9c
izumikatsuko <gsnhj522step@gmail.com>
2023-08-03 10:21:50 +0900

    リストレンダー

--- src/components/LanguageList/index.jsx
+++ src/components/LanguageList/index.jsx
@@ -9,18 +9,39 @@ export const LanguageList = () => {
 
   return (
     <ul className={styles.wrapper}>
-      <li className={styles.item}>
-        <img src={languages[0].imgUrl} />
-        <p>{languages[0].name}</p>
-      </li>
-      <li className={styles.item}>
-        <img src={languages[1].imgUrl} />
-        <p>{languages[1].name}</p>
-      </li>
-      <li className={styles.item}>
-        <img src={languages[2].imgUrl} />
-        <p>{languages[2].name}</p>
-      </li>
+      {
+        languages.map((language) => (
+        <li key={language.id} className={styles.item}>
+          <img src={language.imgUrl} />   {/*リストレンダリングしてる*/}
+          <p>{language.name}</p>   {/*リストレンダリングしてる*/}
+        </li>
+        ))
+      }
     </ul>
   )
 }
+
+// LanguageListコンポーネントでは現在、languages変数のという配列から要素番号を直接指定してそれぞれ表示させています。これをmap()を使用してリストレンダリングさせるように修正してください
+// なお、<li>に指定されているclassName属性や<img>、<p>の構成は変えないこと
+
+// src/App.jsx にLanguageListコンポーネントをインポートして、JSX内に追記し、画面に表示できるようにすること
+
+
+
+///////////////元///////////////
+// return (
+//   <ul className={styles.wrapper}>
+//     <li className={styles.item}>
+//       <img src={languages[0].imgUrl} />
+//       <p>{languages[0].name}</p>
+//     </li>
+//     <li className={styles.item}>
+//       <img src={languages[1].imgUrl} />
+//       <p>{languages[1].name}</p>
+//     </li>
+//     <li className={styles.item}>
+//       <img src={languages[2].imgUrl} />
+//       <p>{languages[2].name}</p>
+//     </li>
+//   </ul>
+// )
