c345d868ad60cfa276fb50b4c027f4f72c823ca0
izumikatsuko <gsnhj522step@gmail.com>
2023-08-03 10:10:12 +0900

    src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Profile } from './components/Profile/index';
 import { Country } from './components/Country/index';
 import { Card } from './components/Card/index';
 import { TodoItem } from './components/TodoItem/index';
+import { LanguageList } from './components/LanguageList/index';
 // ※ src={reactLogo} の書き方はJSX構文の書き方 後ほど...
 // ※ className={styles['react-logo']} の記述はHTMLのclassに当たる書き方 後ほど...
 export const App = () => {
@@ -59,13 +60,16 @@ export const App = () => {
       <>
         <TodoItem />
         <ul>
-
+          {/* 規定記述 */}
           <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
           <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
           <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
         </ul>
       </>
-
+      <hr />{/* 区切り線 */}
+      <>
+        <LanguageList />
+      </>
     </div>
 
   )
