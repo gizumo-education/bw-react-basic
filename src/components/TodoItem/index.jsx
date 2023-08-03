3906f1bdb4ebdaaf33ba04dd6010b3c8f38ea141
izumikatsuko <gsnhj522step@gmail.com>
2023-08-03 10:19:48 +0900

    条件付きレンダー

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -5,7 +5,8 @@ export const TodoItem = ({ name, isToday, isCompleted }) => {
   return (
     <>
       {/* 解答 */}
-      {isToday ? <li className={styles.name}>{name}{isCompleted ? '✔︎' : ''}</li> : ''}
+      {/* [ A  ?                        B(true)                        : C(false) ] */}
+      {isToday ? <li className={styles.name}>{name} {isCompleted ? '✔︎' : ''}</li> : ''}
       {/* <li className={styles.name}>{name}</li> */}
       {/* falseの場合 */}
     </>
@@ -19,6 +20,16 @@ TodoItem.propTypes = {
   isCompleted: PropTypes.bool.isRequired,
 }
 
+////条件
+// ・○src/components/TodoItem/index.jsx のTodoItemコンポーネントに記述すること 
+// ・TodoItemコンポーネントはToDoのリスト1つ1つを出しわけするためのコンポーネントです。
+
+//   - ToDoを今日実施する場合（isToday propsがtrueの場合）のみ、li要素を返し、ToDo名を表示すること
+//   - すでにToDoが完了している場合（isCompleted propsがtrueの場合）、liタグの中で、ToDo名に続けて「✔︎」の文字列をつなげて表示すること（未完了の場合は、ToDo名のみを表示します）
+
+// ・src/App.jsx にTodoItemコンポーネントをインポートし、以下のようにTodoItemコンポーネントを3個追記し、画面に表示された時に、ToDoの出しわけができている状態になっていること
+// 前回までの練習問題で記述した内容は削除せずに追記すること
+
 
 //元
 // export const TodoItem = ({ name, isToday, isCompleted }) => {
@@ -31,15 +42,6 @@ TodoItem.propTypes = {
 //   isCompleted: PropTypes.bool.isRequired,
 // }
 
-////条件
-// ・○src/components/TodoItem/index.jsx のTodoItemコンポーネントに記述すること 
-// ・TodoItemコンポーネントはToDoのリスト1つ1つを出しわけするためのコンポーネントです。
-
-//   - ToDoを今日実施する場合（isToday propsがtrueの場合）のみ、li要素を返し、ToDo名を表示すること
-//   - すでにToDoが完了している場合（isCompleted propsがtrueの場合）、liタグの中で、ToDo名に続けて「✔︎」の文字列をつなげて表示すること（未完了の場合は、ToDo名のみを表示します）
-
-// ・src/App.jsx にTodoItemコンポーネントをインポートし、以下のようにTodoItemコンポーネントを3個追記し、画面に表示された時に、ToDoの出しわけができている状態になっていること
-// 前回までの練習問題で記述した内容は削除せずに追記すること
 
 
 
