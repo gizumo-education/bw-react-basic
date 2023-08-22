622419d4d3c695ff48c63d456e1b5b2fbca4805d
kensansansansan <kensansansansan@gmail.com>
2023-08-22 12:41:28 +0900

    reactセクション4 インポートとエクスポート

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import reactLogo from './assets/react.svg';
 import styles from './App.module.css';
+import { Button } from './components/Button';
 
 export const App = () => {
   return (
@@ -9,6 +10,14 @@ export const App = () => {
       <h1>Hello React!</h1>
 
       {/* 以下に練習問題の回答を記述してください */}
+
+      <hr />
+      <Button />
+      <Button />
+      <Button />
+
+
     </div>
+
   );
 }
