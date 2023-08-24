b667b6922912c3357f27b14205c607bc0a93fb66
SuyamaAyaka <ay.20150013@gmail.com>
2023-08-24 12:58:18 +0900

    Section6 propsを用いて国旗を記載

--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,10 +1,26 @@
 import styles from './index.module.css'
+import PropTypes from 'prop-types'
 
-export const Country = () => {
+// Countryコンポーネント(子)
+export const Country = ({info,width,height}) => {
   return (
     <div>
-      <img src={info.imgUrl} width={width} height={height} />
+      <img
+        src={info.imgUrl}
+        width={width}
+        height={height}
+      />
       <p className={styles.name}>{info.name}</p>
     </div>
   )
 }
+
+Country.PropTypes = {
+  info:PropTypes.exact({
+    imgUrl:PropTypes.string.isRequired,
+    name:PropTypes.string.isRequired
+  }).isRequired,
+  width:PropTypes.number,
+  height:PropTypes.number
+}
+
