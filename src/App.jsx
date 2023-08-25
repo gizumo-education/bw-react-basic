90f3d06cf2d139204394b7bbfcfa9203a06baf30
giz-hiro <hiro.design03@gmail.com>
2023-08-25 12:07:07 +0900

    コンポーネントのインポートとエクスポート練習問題

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
-
+import { Button } from './components/Button'
 
 export const App = () => {
   return (
     <div>
       <img src={reactLogo} className={styles['react-logo']} />
       <h1>Hello React!</h1>
+      <hr />
+      {/* コンポーネントのインポートとエクスポート練習問題 */}
+      <Button />
+      <Button />
+      <Button />
       <hr />
     </div>
   )
