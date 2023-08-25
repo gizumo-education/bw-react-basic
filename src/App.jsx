a8dc6fe12e250127e6ded28507fea5c0de8c0484
giz-hiro <hiro.design03@gmail.com>
2023-08-25 12:13:12 +0900

    条件付きレンダー練習問題

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import styles from './App.module.css'
 import { Button } from './components/Button'
 import { Profile } from './components/Profile'
 import { Country } from './components/Country'
+import { Card } from './components/Card'
+import { TodoItem } from './components/TodoItem'
 
 export const App = () => {
   return (
@@ -28,6 +30,14 @@ export const App = () => {
         />
       </Card>
       <hr />
+      {/* 条件付きレンダー練習問題 */}
+      <ul>
+        <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+        <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+        <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+      </ul>
+      <hr />
+      
     </div>
   )
 }
