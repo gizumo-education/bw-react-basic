4bb53b496e66b7c2be7bc88e65a2508874303b10
giz-hiro <hiro.design03@gmail.com>
2023-08-25 11:48:52 +0900

    フックとuseState練習問題

--- src/components/AlertButton/index.jsx
+++ src/components/AlertButton/index.jsx
@@ -1,5 +1,12 @@
 import styles from './index.module.css'
 
 export const AlertButton = () => {
-  return <button className={styles.button}>Alert</button>
+  const handleAlert = () => {
+    // ボタンがボタンを押した時にアラートを表示させる
+    alert('やあ')
+  }
+  // イベントを適用させたい要素に対して、JSX内で関数を指定する
+  return <button onClick={handleAlert} className={styles.button}>Alert</button>
+
 }
+
--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,10 +1,28 @@
 import styles from './index.module.css'
+import PropTypes from 'prop-types'
 
-export const Country = () => {
+export const Country = ({ info, width, height }) => {
   return (
     <div>
       <img src={info.imgUrl} width={width} height={height} />
       <p className={styles.name}>{info.name}</p>
     </div>
   )
+}
+
+Country.propTypes = {
+  // infoがexact()の中に定義するオブジェクトの形で必ず渡す。
+  info: PropTypes.exact({
+    imgUrl: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+  }).isRequired,
+  // 任意でwidth,height propsを渡した場合、number型の値を渡すことを定義
+  width: PropTypes.number,
+  height: PropTypes.numberx
+}
+
+// propsのデフォルト値
+Country.defaultProps = {
+  width: 120,
+  height: 80
 }
