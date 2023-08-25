edc582036794fdd851837a7bb9f0bb9097bf0717
giz-hiro <hiro.design03@gmail.com>
2023-08-25 18:04:05 +0900

    条件付きレンダー練習問題（修正）

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,9 +2,7 @@ import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
 export const TodoItem = ({ name, isToday, isCompleted }) => {
-  if (isToday) {
-    return <li className={styles.name}>{isCompleted ? name + '✔︎' : name}</li>
-  }
+  return isToday && <li className={styles.name}>{isCompleted ? name + '✔︎' : name}</li>
 }
 
 TodoItem.propTypes = {
