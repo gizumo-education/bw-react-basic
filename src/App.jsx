0d9f5ecb2b6d09d1aeb310590fbd3929efbc4b5c
giz-hiro <hiro.design03@gmail.com>
2023-08-25 12:19:43 +0900

    イベントハンドラ練習問題

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Country } from './components/Country'
 import { Card } from './components/Card'
 import { TodoItem } from './components/TodoItem'
 import { LanguageList } from './components/LanguageList'
+import { AlertButton } from './components/AlertButton'
 
 export const App = () => {
   return (
@@ -40,6 +41,9 @@ export const App = () => {
       <hr />
       {/* リストレンダー練習問題 */}
       <LanguageList />
+      <hr />
+      {/* イベントハンドラ練習問題 */}
+      <AlertButton />
       <hr />
     </div>
   )
