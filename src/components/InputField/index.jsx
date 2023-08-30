108df4b3c7254b762f2f9bc01cd53f47470669f6
Ibuki8 <ibukixox@gmail.com>
2023-08-30 15:43:35 +0900

    インデント修正

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,15 +1,19 @@
-import styles from './index.module.css'
-import { useState } from 'react'
+import styles from "./index.module.css";
+import { useState } from "react";
 
 export const InputField = () => {
-  const[value,setValue] = useState('')
+  const [value, setValue] = useState("");
   const handleChange = (e) => {
-    setValue(e.target.value)
+    setValue(e.target.value);
   }
 
   return (
     <>
-      <input onChange={handleChange} className={styles.input} placeholder='任意の文字を入力' />
+      <input
+        onChange={handleChange}
+        className={styles.input}
+        placeholder="任意の文字を入力"
+      />
       <p>入力文字：{value}</p>
     </>
   )
