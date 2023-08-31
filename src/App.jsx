046dc0944dc40f7915e0397911f5005db8c77f64
Tomoyuki Tanak <tomoyukitanaka@TomoyukinoMacBook-Pro.local>
2023-08-31 14:36:57 +0900

    section6_練習問題

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import Button from './components/Button/index.jsx'
 import { Profile } from './components/Profile/index.jsx'
+import { Country } from './components/Country/index.jsx'
+import { Card } from './components/Card/index.jsx'
 
 export const App = () => {
   return (
@@ -16,6 +18,22 @@ export const App = () => {
     <Button />
     <hr />
     <Profile />
+    <hr />
+    <Country
+      info={{
+        imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+        name: 'アメリカ'
+      }}
+    />
+    <hr />
+    <Card>
+      <Country
+        info={{
+          imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+          name: 'アメリカ'
+        }}
+      />
+    </Card>
   </div>
   )
 }
--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,6 +1,7 @@
 import styles from './index.module.css'
+import PropTypes from 'prop-types'
 
-export const Country = () => {
+export const Country = ({ info, width, height }) => {
   return (
     <div>
       <img src={info.imgUrl} width={width} height={height} />
@@ -8,3 +9,17 @@ export const Country = () => {
     </div>
   )
 }
+
+Country.defaultProps = {
+  width: 120,
+  height: 80
+}
+
+Country.propTypes = {
+  info: PropTypes.exact({
+    imgUrl:PropTypes.string.isRequired,
+    name:PropTypes.string.isRequired
+    }).isRequired,
+  with:PropTypes.number,
+  height:PropTypes.number
+  }
