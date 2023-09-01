d8f2d0dc1a948962afbc53d56c150a118b1bd4e4
itowataru2024 <itawatru2@gmail.com>
2023-09-01 14:08:37 +0900

    文章とチェックマークを追加しました

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Button } from './components/Button';
 import { Profile } from './components/Profile';
 import { Country } from './components/Country';
 import { Card } from './components/Card';
-
+import { TodoItem } from './components/TodoItem';
 
 <div>
       <img src={reactLogo} className={styles['react-logo']} />
@@ -28,6 +28,12 @@ import { Card } from './components/Card';
           <Card >
             <Country info={{imgUrl:'https://www.civillink.net/sozai/kokki100/pics2654.gif',name:'アメリカ'}}/>
           </Card>
+          <hr />
+          <ul>
+           <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+           <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+           <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+          </ul>
         </div>
       )
     };
