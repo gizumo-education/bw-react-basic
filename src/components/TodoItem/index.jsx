13e8429cc194a2adefe1460080f50c80fb8b8322
nanami_asakawa <nanamiasakawa77@gmail.com>
2023-09-06 14:42:35 +0900

    ReactLesson7練習問題

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,8 +1,15 @@
 import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
+// export const TodoItem = ({ name, isToday, isCompleted }) => {
+//   return <li className={styles.name}>{name}</li>
+// }
+
 export const TodoItem = ({ name, isToday, isCompleted }) => {
-  return <li className={styles.name}>{name}</li>
+  if (!isToday) {
+    return null
+  }
+  return <li className={styles.name}>{name} {isCompleted ? '✔︎' : null}</li>
 }
 
 TodoItem.propTypes = {
@@ -10,3 +17,4 @@ TodoItem.propTypes = {
   isToday: PropTypes.bool.isRequired,
   isCompleted: PropTypes.bool.isRequired,
 }
+
