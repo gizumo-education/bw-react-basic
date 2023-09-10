13493612228906f66f47070ca31c385460c797c8
nanami_asakawa <nanamiasakawa77@gmail.com>
2023-09-11 05:36:02 +0900

    ReactLesson8map

--- src/components/LanguageList/index.jsx
+++ src/components/LanguageList/index.jsx
@@ -1,5 +1,30 @@
 import styles from './index.module.css'
 
+// export const LanguageList = () => {
+//   const languages = [
+//     { id: 1, name: 'HTML', imgUrl: 'https://img.icons8.com/color/48/html-5--v1.png' },
+//     { id: 2, name: 'JavaScript', imgUrl: 'https://img.icons8.com/color/48/javascript--v1.png' },
+//     { id: 3, name: 'TypeScript', imgUrl: 'https://img.icons8.com/color/48/typescript.png' },
+//   ]
+
+//   return (
+//     <ul className={styles.wrapper}>
+//       <li className={styles.item}>
+//         <img src={languages[0].imgUrl} />
+//         <p>{languages[0].name}</p>
+//       </li>
+//       <li className={styles.item}>
+//         <img src={languages[1].imgUrl} />
+//         <p>{languages[1].name}</p>
+//       </li>
+//       <li className={styles.item}>
+//         <img src={languages[2].imgUrl} />
+//         <p>{languages[2].name}</p>
+//       </li>
+//     </ul>
+//   )
+// }
+
 export const LanguageList = () => {
   const languages = [
     { id: 1, name: 'HTML', imgUrl: 'https://img.icons8.com/color/48/html-5--v1.png' },
@@ -9,18 +34,15 @@ export const LanguageList = () => {
 
   return (
     <ul className={styles.wrapper}>
-      <li className={styles.item}>
-        <img src={languages[0].imgUrl} />
-        <p>{languages[0].name}</p>
-      </li>
-      <li className={styles.item}>
-        <img src={languages[1].imgUrl} />
-        <p>{languages[1].name}</p>
-      </li>
-      <li className={styles.item}>
-        <img src={languages[2].imgUrl} />
-        <p>{languages[2].name}</p>
-      </li>
+      {
+        languages.map((language) => (
+          <li className={styles.item} key={language.id}>
+            <img src={language.imgUrl}></img>
+            <p>{language.name}</p>
+          </li>
+        ))
+      }
     </ul>
   )
+
 }
