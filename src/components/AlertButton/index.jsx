ed694740d45a77f6f45b962f78169dfdf871fc58
nanami_asakawa <nanamiasakawa77@gmail.com>
2023-09-11 05:55:33 +0900

    ReactLesson9 handle

--- src/components/AlertButton/index.jsx
+++ src/components/AlertButton/index.jsx
@@ -1,5 +1,12 @@
 import styles from './index.module.css'
 
+// export const AlertButton = () => {
+//   return <button className={styles.button}>Alert</button>
+// }
+
 export const AlertButton = () => {
-  return <button className={styles.button}>Alert</button>
+  const handleAlart = () => {
+    alert('SpiderMan')
+  }
+  return <button className={styles.button} onClick={handleAlart}>Alert</button>
 }
