9bc1fd610a344ca2eac3007290f932dd3bcccd68
IshikawaTokyo <yyusb.mac@gmail.com>
2023-09-13 17:14:28 +0900

    section7の練習問題を解きました

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,12 +1,18 @@
-import PropTypes from 'prop-types'
-import styles from './index.module.css'
+import PropTypes from "prop-types";
+import styles from "./index.module.css";
 
 export const TodoItem = ({ name, isToday, isCompleted }) => {
-  return <li className={styles.name}>{name}</li>
-}
+  return (
+    <>
+      {isToday ? (
+        <li className={styles.name}>{isCompleted ? name + "✔︎" : name}</li>
+      ) : null}
+    </>
+  );
+};
 
 TodoItem.propTypes = {
   name: PropTypes.string.isRequired,
   isToday: PropTypes.bool.isRequired,
   isCompleted: PropTypes.bool.isRequired,
-}
+};
