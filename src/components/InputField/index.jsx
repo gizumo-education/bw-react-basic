e1859a06ecdcb91e41948c23b373606c886fd2f7
nanami_asakawa <nanamiasakawa77@gmail.com>
2023-09-13 12:54:24 +0900

    ReactLesson10 setState

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react'
 import styles from './index.module.css'
 
+// export const InputField = () => {
+//   let value = ''
+
+//   return (
+//     <>
+//       <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
+//       <p>入力文字：{value}</p>
+//     </>
+//   )
+// }
+
 export const InputField = () => {
-  let value = ''
+  const [value, setValue]= useState('')
+  const handleValue = (e) => {
+    setValue(e.target.value)
+  }
 
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
+      <input onChange={handleValue} className={styles.input} placeholder='任意の文字を入力' />
       <p>入力文字：{value}</p>
     </>
   )
