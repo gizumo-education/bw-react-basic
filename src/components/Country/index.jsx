f1cd53355f1bdcf01c09e03410e490509d8d73d8
itowataru2024 <itawatru2@gmail.com>
2023-09-14 20:41:22 +0900

    コードレビューの箇所を修正致しました

--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,11 +1,22 @@
 import styles from './index.module.css'
-
+import PropTypes from 'prop-types'
 
 export const Country = ({info}) => {
   return (
     <div>
-      <img src={info.imgUrl} width={info.width} height={info.height} />
+      <img src={info.imgUrl} width='150px' height='100px'/>
       <p className={styles.name}>{info.name}</p>
     </div>
   )
+}
+
+Country.propTypes = {
+  info:PropTypes.exact({
+    imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+    name: 'アメリカ'
+  })
+};
+Country.defaultProps = {
+  width:120,
+  height:80
 }
