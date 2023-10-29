2ae69d32979df2c772dfaf58fe7e499518783fe7
sakigig <o0daifuku0o@gmail.com>
2023-10-29 17:25:32 +0900

    React section4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
-
+import { Button } from './components/Button'
 
 
 export const App = () => {
@@ -8,6 +8,11 @@ export const App = () => {
     <div>
       <img src={reactLogo} className={styles['react-logo']} />
       <h1>Hello React!</h1>
+
+      <hr />
+      <Button />
+      <Button />
+      <Button />
 
       {/* 以下に練習問題の回答を記述してください */}
     </div>
