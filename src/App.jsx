55bfb55ca24f426fa4ab24fa4b54b910ddb9d3a2
sakigig <o0daifuku0o@gmail.com>
2023-10-30 10:56:45 +0900

    React section5

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import { Button } from './components/Button'
+import { Profile } from './components/Profile'
+// index.jsからnamed exportでエクスポートしたので、{}をつけて変数Buttonをインポート
+// モジュールバンドラーで拡張子を短縮（/index.jsxを省略）
 
 
 export const App = () => {
@@ -9,12 +12,20 @@ export const App = () => {
       <img src={reactLogo} className={styles['react-logo']} />
       <h1>Hello React!</h1>
 
+      {/* 以下に練習問題の回答を記述してください */}
+
       <hr />
+
       <Button />
       <Button />
       <Button />
 
-      {/* 以下に練習問題の回答を記述してください */}
+      <hr />
+
+      <Profile />
+
     </div>
   )
 }
+
+// インポートしたコンポーネントを<Button />という記述で利用
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,3 +3,8 @@ import styles from './index.module.css'
 export const Button = () => {
 return <button className={styles.button}>Button</button>
 };
+
+// exportの後ろに記述してコンポーネントをエクスポート
+// 変数Buttonにアロー関数（functionが=>、引数はなしなので()、）を使ってマークアップを記述
+// マークアップ：returnと同じ行にマークアップが収まらない場合は、()で囲む
+// 今回は１行で収まっているので（）なし
