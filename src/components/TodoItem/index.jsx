be07b86c46260fa00803aa1bc2baef55885291b7
sakigig <o0daifuku0o@gmail.com>
2023-10-30 14:39:04 +0900

    React section7

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,8 +2,18 @@ import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
 export const TodoItem = ({ name, isToday, isCompleted }) => {
-  return <li className={styles.name}>{name}</li>
-}
+  return (
+    <li className={styles.name}>
+      {isCompleted ? (
+        <>
+          {name}✔︎
+        </>
+      ) : isToday ? (
+        name
+      ) : null}
+      </li>
+  );
+};
 
 TodoItem.propTypes = {
   name: PropTypes.string.isRequired,
