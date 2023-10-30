3c2fcd3475978fce01134b0a2cf8f90cb1728d68
sakigig <o0daifuku0o@gmail.com>
2023-10-30 16:37:41 +0900

    React section11

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Card } from './components/Card'
 import { TodoItem } from './components/TodoItem'
 import { LanguageList } from './components/LanguageList'
 import { AlertButton } from './components/AlertButton'
+import { InputField } from './components/InputField'
 
 
 // index.jsからnamed exportでエクスポートしたので、{}をつけて変数Buttonをインポート
@@ -50,7 +51,13 @@ export const App = () => {
       <LanguageList />
 
       <hr />
+
       <AlertButton />
+
+      <hr />
+      
+      <InputField />
+
     </div>
   )
 }
--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,12 +1,30 @@
 import styles from './index.module.css'
+import { useState } from 'react';
 
 export const InputField = () => {
-  let value = ''
+  // let value = ''
+  const [inputValue, setInputValue] = useState('');
+  const InputChange = (e) => {
+    // 入力値を取得してinputValueを更新
+    setInputValue(e.target.value);
+  };
+
 
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
-      <p>入力文字：{value}</p>
+      {/* <input onChange={(e) => value = e.target.value} 
+      // <input>要素に対してonChangeイベントハンドラを設定
+      // このイベントハンドラは、アロー関数を使用して定義されており、入力イベントオブジェクト(e)から入力された値(e.target.value)を取得し、valueという変数に代入している
+      className={styles.input} placeholder='任意の文字を入力' /> */}
+      <input
+        type="text"
+        value={inputValue}
+        onChange={InputChange}
+        className={styles.input}
+        placeholder='任意の文字を入力'
+      />
+      <p>入力文字：{inputValue}</p>
+      {/* <p>入力文字：{value}</p> */}
     </>
   )
 }
