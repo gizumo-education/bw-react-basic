32a18fd86c97aa97836014f570d36bf734a32f30
kobayashi <citcho716@gmail.com>
2023-11-03 20:44:11 +0900

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { TodoItem } from './components/TodoItem'
 import { LanguageList } from './components/LanguageList'
 import { AlertButton } from './components/AlertButton'
 import { InputField } from './components/InputField'
+import { CharacterImage } from './components/CharacterImage'
 
 export const App = () => {
   return (
@@ -70,6 +71,10 @@ export const App = () => {
       <hr />
 
       <InputField />
+
+      <hr />
+
+      <CharacterImage />
     </div>
   )
 }
