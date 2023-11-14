678d43a048cc049558ac0bb54cbc055e4cdb8076
kazushigeyamauti <zushizushi331@gmail.com>
2023-11-14 20:14:44 +0900

    Section4問題1

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,10 @@
 import styles from './index.module.css'
 
-export default function Button() {
+export const Button = () => {
   return (
     <button className={styles.button}>Button</button>
   )
 }
+
+// 真っ白になったらコンソールを見る
+// エラーメッセージを確認する
