57251a533e2e965aa7f72f6a38f3bc94d24ce02c
Yusaku0329 <nakamura.rizumu.nukesaku74@gmail.com>
2023-11-20 18:10:58 +0900

    add:InputFieldコンポーネントを追加

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,12 +1,17 @@
 import styles from './index.module.css'
+import { useState } from 'react'
+
 
 export const InputField = () => {
-  let value = ''
+  const [inValue, setValue] = useState('')
+  const handleInput = (e) => {
+    setValue(e.target.value )
+  }
 
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
-      <p>入力文字：{value}</p>
+      <input value={inValue} onChange={handleInput} className={styles.input} placeholder='任意の文字を入力' />
+      <p>入力文字：{inValue}</p>
     </>
   )
 }
