fdcc0512e27acebc9bcc88afcb7ac2b307e0b0bd
tatsuhiroto615 <tatsuhi.revolution@gmail.com>
2023-11-21 09:57:19 +0900

    Profileコンポーネントの修正

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,7 +1,7 @@
 import styles from './index.module.css'
 
 export const Profile = () => {
-  const imgUrl = 'https://i.imgur.com/7vQD0fPs.jpg';
+  const imgUrl = 'https://i.imgur.com/7vQD0fPs.jpg'
   const name = 'John'
   const getBirthDay = () => {
     return new Date().toLocaleDateString()
