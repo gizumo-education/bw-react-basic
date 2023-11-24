83b2358930ac7449e18b388d6d7625aa99a5af6d
taichi-watabe <124047993+taichi-watabe@users.noreply.github.com>
2023-11-24 18:30:10 +0900

    section7の練習問題の実装

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,6 +2,12 @@ import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
 export const TodoItem = ({ name, isToday, isCompleted }) => {
+  if(!isToday) {
+    return null
+  }
+  if(isCompleted) {
+    return <li className={styles.name}>{name + '✔︎'}</li>
+  }
   return <li className={styles.name}>{name}</li>
 }
 
