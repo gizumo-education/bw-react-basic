4771588ca7d269744d139574e8f6ce24f6941904
Yusaku0329 <nakamura.rizumu.nukesaku74@gmail.com>
2023-11-24 10:44:17 +0900

    fix:Buttonコンポーネントのスタイルを修正

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,6 @@
-import styles from './index.module.css'
-import clsx from 'clsx'
 
-export const Button = ({ className, isActive }) => {
-  return <button className={clsx(className, styles.button, {[styles.active]:isActive})}>Click me</button>
+import styles from './index.module.css';
+
+export const Button = () => {
+  return <button className={styles.button}>Click me</button>
 }
