8c5c0567cb0908590d4f2ef130f20bc1e4bfdff3
taichi-watabe <124047993+taichi-watabe@users.noreply.github.com>
2023-11-27 16:41:21 +0900

    Section10の練習問題の実装

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Card } from './components/Card'
 import { TodoItem } from './components/TodoItem'
 import { LanguageList } from './components/LanguageList'
 import { AlertButton } from './components/AlertButton'
+import { InputField } from './components/InputField'
 
 export const App = () => {
 
@@ -46,6 +47,8 @@ return(
       <LanguageList />
     <hr />
       <AlertButton />
+    <hr />
+      <InputField />
   </div>
   )
 }
