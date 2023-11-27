718f69daee872c445fd27ca214bc5ee36efab074
taichi-watabe <124047993+taichi-watabe@users.noreply.github.com>
2023-11-27 10:30:31 +0900

    Section8の練習問題の実装

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Profile } from './components/Profile'
 import { Country } from './components/Country'
 import { Card } from './components/Card'
 import { TodoItem } from './components/TodoItem'
+import { LanguageList } from './components/LanguageList'
 
 export const App = () => {
 
@@ -40,6 +41,8 @@ return(
       <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
       <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
     </ul>
+    <hr />
+      <LanguageList />
   </div>
   )
 }
