277da077a448e9e25047a941edf807e564f2df2c
渡邊暖 <ataru@MacBook-Air.local>
2023-12-25 10:28:57 +0900

    section6 card追加 & section5 error修正

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import { Button } from './components/Button/index.jsx'
 import { Profile } from './components/Profile/index.jsx';
+import { Country } from './components/Country/index.jsx';
+import { Card } from './components/Card/index.jsx';
 
 export const App = () => {
   return (
@@ -19,6 +21,17 @@ export const App = () => {
 
       <hr />
       <Profile />
+
+      <hr />
+      <Card>
+        <Country
+          info = {{
+          imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+          name: 'アメリカ'
+          }}
+        />
+      </Card>
+        
     </div>
   )
 };
