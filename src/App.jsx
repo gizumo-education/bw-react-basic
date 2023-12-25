62776433293527ae2373027fa83f37ed20fecd1e
渡邊暖 <ataru@MacBook-Air.local>
2023-12-25 18:22:57 +0900

    section10 Inputfield追加

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Card } from './components/Card/index.jsx';
 import { TodoItem } from './components/TodoItem/index.jsx';
 import { LanguageList } from './components/LanguageList/index.jsx';
 import { AlertButton } from './components/AlertButton/index.jsx';
+import { InputField } from './components/InputField/index.jsx';
 
 export const App = () => {
   return (
@@ -48,6 +49,8 @@ export const App = () => {
       <hr />
       <AlertButton />
 
+      <hr />
+      <InputField />
     </div>
   )
 };
