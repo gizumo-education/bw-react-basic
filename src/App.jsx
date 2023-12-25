a1d00fa7bc3d301d0d4243a84c2d28a0bfec3f42
渡邊暖 <ataru@MacBook-Air.local>
2023-12-25 12:28:55 +0900

    section7 TodoItem追加

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Button } from './components/Button/index.jsx'
 import { Profile } from './components/Profile/index.jsx';
 import { Country } from './components/Country/index.jsx';
 import { Card } from './components/Card/index.jsx';
+import { TodoItem } from './components/TodoItem/index.jsx';
 
 export const App = () => {
   return (
@@ -25,13 +26,20 @@ export const App = () => {
       <hr />
       <Card>
         <Country
-          info = {{
-          imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
-          name: 'アメリカ'
+          info={{
+            imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+            name: 'アメリカ'
           }}
         />
       </Card>
-        
+
+      <hr />
+      <ul>
+        <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+        <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+        <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+      </ul>
+
     </div>
   )
 };
