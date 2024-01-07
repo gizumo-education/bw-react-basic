411acc93f43f3e50492f3069523106f2661ac7a7
ayakatakano <ayakatakano0714@gmail.com>
2024-01-07 18:04:55 +0900

    Section7 ToDo表示

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,7 +2,12 @@ import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
 export const TodoItem = ({ name, isToday, isCompleted }) => {
-  return <li className={styles.name}>{name}</li>
+  if (!isToday) {
+    return null
+  }
+  return (
+    isCompleted ? <li className={styles.name}>{name}✓</li> : <li className={styles.name}>{name}</li>
+  )  
 }
 
 TodoItem.propTypes = {
