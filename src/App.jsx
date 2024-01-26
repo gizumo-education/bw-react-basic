9ae7073122530624e38b7e05fd289eabe6ae555a
Odawara Reina <odawara.reina.1107@gmail.com>
2024-01-26 16:44:02 +0900

    section3の練習問題

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,12 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
+
+
+export const App = () => {
+  return (
+    <div>
+      <img src={reactLogo} className={styles['react-logo']} />
+      <h1>Hello React!</h1>
+    </div>
+  )
+}
