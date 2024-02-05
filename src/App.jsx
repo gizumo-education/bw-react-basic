587d061a364748cb138e2c1a5df9140a81a32836
椎名胡桃 <shiinakurumi@shiinakuruminonotobukkukonpyuta.local>
2024-02-05 14:09:04 +0900

    Alertボタンの追加

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Country } from './components/Country'
 import { Card } from './components/Card'
 import { TodoItem } from './components/TodoItem'
 import { LanguageList } from './components/LanguageList'
+import { AlertButton } from './components/AlertButton'
 
 export const App = () => {
   return (
@@ -43,6 +44,9 @@ export const App = () => {
 
       <hr />
       <LanguageList />
+
+      <hr />
+      <AlertButton />
     </div>
   )
 }
