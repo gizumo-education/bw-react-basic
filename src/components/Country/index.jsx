af6da2f5329885c83b365aea32646c897c930db8
Odawara Reina <odawara.reina.1107@gmail.com>
2024-04-05 16:28:03 +0900

    練習問題実装

--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,10 +1,28 @@
 import styles from './index.module.css'
+import PropTypes from 'prop-types'
 
-export const Country = () => {
+export const Country = ({info, size}) => {
   return (
     <div>
-      <img src={info.imgUrl} width={width} height={height} />
+      <img
+        src={info.imgUrl}
+        width={size}
+        height={size}
+      />
       <p className={styles.name}>{info.name}</p>
     </div>
   )
 }
+
+Country.propTypes = {
+  info: PropTypes.exact({
+  url: PropTypes.string.isRequired
+  }).isRequired,
+  size: PropTypes.number
+}
+  Country.defaultProps= {
+    size: {
+      width: 120,
+      height: 80,
+    }
+  }
--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,11 +1,19 @@
 import styles from './index.module.css'
+import {useState} from 'react'
 
 export const InputField = () => {
-  let value = ''
 
+  const [value, setValue] = useState('')
+  const handleChange = (e) =>{
+  setValue(e.target.value)
+  }
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
+      <input
+      onChange={handleChange}
+      className={styles.input}
+      placeholder='任意の文字を入力'
+      ></input>
       <p>入力文字：{value}</p>
     </>
   )
