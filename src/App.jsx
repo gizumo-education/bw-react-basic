539e0c92136adffbee775ee9a2e161c2d8ccad16
Maisaka564 <m9016r@gmail.com>
2024-04-09 13:40:18 +0900

    feat:InputFieldを作成した。

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Card } from "./components/Card";
 import { TodoItem } from "./components/TodoItem";
 import { LanguageList } from "./components/LanguageList";
 import { AlertButton } from "./components/AlertButton";
+import { InputField } from "./components/InputField";
 
 export const App = () => {
   return (
@@ -38,9 +39,9 @@ export const App = () => {
 
       <hr />
 
-      <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
-      <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
-      <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+      <TodoItem name="Reactの学習" isToday={true} isCompleted={true} />
+      <TodoItem name="HTMLの復習" isToday={true} isCompleted={false} />
+      <TodoItem name="React Hooksの予習" isToday={false} isCompleted={false} />
 
       <hr />
       <LanguageList />
@@ -48,6 +49,9 @@ export const App = () => {
       <hr />
       <AlertButton />
 
+      <hr />
+
+      <InputField />
     </div>
   );
 };
