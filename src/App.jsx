f113ba6f356d661f877b7dff154b463424f6b826
Todatomoyuki <tomoyuki.jam1@gmail.com>
2024-04-16 14:59:23 +0900

    add レダリング要素をブラウザに追加

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,24 @@ import { Button } from './components/Button';
 import { Profile } from './components/Profile';
 import { Country } from './components/Country';
 import { Card } from './components/Card';
+import { TodoItem } from './components/TodoItem';
 
 export const App = () => {
   return (
     <div>
       <img src={reactLogo} className={styles['react-logo']} />
       <h1>Hello React!</h1>
+
       <hr />
       {/* <!-- Buttonコンポーネントのタグをここに3つ記述 --> */}
       <Button />
       <Button />
       <Button />
+
       <hr />
       {/* <!-- Profileコンポーネントのタグをここに記述 --> */}
       <Profile />
+
       <hr />
       {/* <!-- CardコンポーネントとCountryコンポーネントのタグをここに記述 --> */}
       <Card>
@@ -28,7 +32,16 @@ export const App = () => {
         imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
         name: 'アメリカ'
       }}
-    /></Card>
+      />
+      </Card>
+
+      <hr />
+      {/* <!-- ulタグとTodoItemコンポーネントのタグをここに記述 --> */}
+      <ul>
+        <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+        <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+        <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+      </ul>
     </div>
   )
 };
