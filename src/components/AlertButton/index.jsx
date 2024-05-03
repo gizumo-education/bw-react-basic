e38f41712d8df99bc454cb3a828aeae31abc4740
akiho_matsymoto <a-matsumotogmail.com>
2024-05-03 16:55:49 +0900

    section9

--- src/components/AlertButton/index.jsx
+++ src/components/AlertButton/index.jsx
@@ -1,5 +1,5 @@
 import styles from './index.module.css'
 
 export const AlertButton = () => {
-  return <button className={styles.button}>Alert</button>
+  return <button className={styles.button} onClick={() => alert('ネコ大暴れ！')}>Alert</button>
 }
