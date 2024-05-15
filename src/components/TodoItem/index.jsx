34d8ee227f499d061a6cc90c651f9f0e0596bb8e
atsuikeda <ike.ke.no.ke@gmail.com>
2024-05-15 16:20:42 +0900

    React_section7 条件付きカレンダー　練習問題解答

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,12 +1,20 @@
-import PropTypes from 'prop-types'
-import styles from './index.module.css'
+import PropTypes from "prop-types";
+import styles from "./index.module.css";
 
 export const TodoItem = ({ name, isToday, isCompleted }) => {
-  return <li className={styles.name}>{name}</li>
-}
+  return (
+    <>
+      {isToday && (
+        <li className={styles.name}>
+          {isCompleted ? <>{`${name}✔︎`}</> : <>{name}</>}
+        </li>
+      )}
+    </>
+  );
+};
 
 TodoItem.propTypes = {
   name: PropTypes.string.isRequired,
   isToday: PropTypes.bool.isRequired,
   isCompleted: PropTypes.bool.isRequired,
-}
+};
