81dc2ddd272fd790d10d822841a29dc0aa808770
atsuikeda <ike.ke.no.ke@gmail.com>
2024-05-15 16:59:44 +0900

    React section9 イベントハンドラ　練習問題解答

--- src/components/AlertButton/index.jsx
+++ src/components/AlertButton/index.jsx
@@ -1,5 +1,10 @@
 import styles from './index.module.css'
 
 export const AlertButton = () => {
-  return <button className={styles.button}>Alert</button>
+
+  const handleAlert= () => {
+    alert('hoge')
+  }
+
+  return <button onClick={handleAlert} className={styles.button}>Alert</button>
 }
