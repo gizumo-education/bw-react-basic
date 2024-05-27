b4974738b66fcbfa44b1c251274e75e34ca17b52
yusuke0927 <gorirando0927@gmail.com>
2024-05-28 07:39:46 +0900

    Lesson5以降の問題解答

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import {Button} from './components/Button'
+import {Profile} from './components/Profile'
+import {Country} from './components/Country'
+import {Card} from './components/Card'
+import {TodoItem} from './components/TodoItem'
+import {LanguageList} from './components/LanguageList'
+import {AlertButton} from './components/AlertButton'
+import {InputField} from './components/InputField'
+import {CharacterImage} from './components/CharacterImage'
 
 export const App = () => {
   return (
@@ -12,7 +20,30 @@ export const App = () => {
       <hr />
       <Button /><Button /><Button />
       <hr />
-      
+      <Profile />
+      <hr />
+      <Card>
+        <Country 
+          info = {{
+            imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+            name: 'アメリカ'
+          }}
+        />
+      </Card>
+      <hr />
+      <ul>
+        <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+        <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+        <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+      </ul>
+      <hr />
+      <LanguageList />
+      <hr />
+      <AlertButton />
+      <hr />
+      <InputField />
+      <hr />
+      <CharacterImage />
     </div>
   )
 }
--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,12 +1,21 @@
 import styles from './index.module.css'
+import { useState } from 'react'
 
 export const InputField = () => {
-  let value = ''
+  const [text, setText] = useState("")
 
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
-      <p>入力文字：{value}</p>
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} className={styles.input} placeholder='任意の文字を入力' />
+      <p>入力文字：{text}</p>
     </>
   )
 }
+
+
+// return (
+//   <>
+//     <input onChange={(e) => setValue = (e.target.value)} className={styles.input} placeholder='任意の文字を入力' />
+//     <p>入力文字：{value}</p>
+//   </>
+// )
