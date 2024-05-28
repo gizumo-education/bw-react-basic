1cb63ddcf5e1357e1e0b459bc31e70f5267729b2
isekana <njtpjgpmad@gmail.com>
2024-05-28 19:33:19 +0900

    Section6

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,32 @@ import styles from './App.module.css'
 import { Button } from './components/Button/index'
 //Section5 
 import { Profile } from './components/Profile/index'
+import { Country } from './components/Country'
+import { children } from 'react'
+import { Card } from './components/Card/index'
 
 export const App = () => {
   return (
     <div>
-<img src={reactLogo} className={styles['react-logo']} />
-<h1>Hello React!</h1>
-{/* 以下に練習問題の回答を記述してください */} 
-<hr />
-<Button/><Button/><Button/>
-<hr />
-<Profile/>
+      <img src={reactLogo} className={styles['react-logo']} />
+      <h1>Hello React!</h1>
+      {/* 以下に練習問題の回答を記述してください */} 
+      <hr />
+      <Button/><Button/><Button/>
+      <hr />
+      <Profile/>
+      <hr />
+      <Card>
+        <Country
+          info={{
+            imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+            name: 'アメリカ'
+          }}
+          width={120}
+          height={80}
+        />
+      </Card>
+     
 </div>
   )
 }
