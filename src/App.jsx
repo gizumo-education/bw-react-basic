bc63b31c3bdc9880f224b4b6434f7dbeaa9c04de
高椋勇輔 <takamukuyusuke@takamukuisamutasukunoMacBook-Pro.local>
2024-05-29 17:59:20 +0900

    section5

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/react.svg';
 import styles from './App.module.css';
+import { Button } from './components/Button';
+import { Profile } from  './components/Profile';
 
 export const App = () => {
   return (
     <div>
       <img src={reactLogo} className={styles['react-logo']} />
       <h1>Hello React!</h1>
+      <hr />
 
-      {/* 以下に練習問題の回答を記述してください */}
+      <Button />
+      <Button />
+      <Button />
+      <hr />
+      <Profile />
+
+
     </div>
   );
 };
