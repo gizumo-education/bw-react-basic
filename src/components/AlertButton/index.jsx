e9c9e3fa9da1aea728c1a7889b0ffd59e92afb17
koutorino <tako.omg@gmail.com>
2024-06-06 18:35:42 +0900

    feat: propstypeを追加

--- src/components/AlertButton/index.jsx
+++ src/components/AlertButton/index.jsx
@@ -1,6 +1,11 @@
 import styles from './index.module.css'
+import PropTypes from 'prop-types';
 
 
 export const AlertButton = ({ onAlert }) => {
   return <button className={styles.button} onClick={ onAlert }>Alert</button>
 }
+
+AlertButton.propTypes = {
+  onAlert: PropTypes.func.isRequired,
+};
