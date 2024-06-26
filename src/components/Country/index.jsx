eab2b816aa4dc063dcdfb8ef249a8e2f000c3084
Rei Kadosawa <rei@ReinoMacBook-Pro.local>
2024-06-26 16:41:48 +0900

    国旗の追加

--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,10 +1,25 @@
 import styles from './index.module.css'
+import PropTypes from 'prop-types'
 
-export const Country = () => {
+export const Country = ({info, height, width, children}) => {
   return (
     <div>
       <img src={info.imgUrl} width={width} height={height} />
       <p className={styles.name}>{info.name}</p>
     </div>
   )
+}
+
+Country.propTypes = {
+  info: PropTypes.shape({
+    imgUrl: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  height: PropTypes.number,
+  width: PropTypes.number,
+  children: PropTypes.node,
+};
+
+Country.defaultProps = {
+  width: 120, height: 80
 }
