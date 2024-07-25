b3b27781ea27db6a70bb6e5af6f1318b1d009294
大塚瞳 <otsukahitomi@ootsukahitominoMacBook-Air.local>
2024-07-25 15:37:15 +0900

    Section8 リストレンダー

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {Profile} from './components/Profile/index'
 import { Country } from './components/Country'
 import { Card } from './components/Card'
 import { TodoItem } from './components/TodoItem'
+import { LanguageList } from './components/LanguageList'
 
 export const App = () => {
   return (
@@ -41,6 +42,17 @@ export const App = () => {
   <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
   <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
 </ul>
+
+<hr />
+<LanguageList />
     </div>
+
   )
 }
+
+// src/components/LanguageList/index.jsx のLanguageListコンポーネントに記述すること
+// LanguageListコンポーネントでは現在、languages変数の配列から要素番号を直接指定してそれぞれ表示させています。これをmap()を使用してリストレンダリングさせるように修正してください
+// なお、<li>に指定されているclassName属性や<img>、<p>の構成は変えないこと
+// src/App.jsx にLanguageListコンポーネントをインポートして、JSX内に追記し、画面に表示できるようにすること
+// 前回までの練習問題で記述した内容は削除せずに追記すること
+// 以下のように<hr />の記述を追加して区切り線をつけ、見やすい形にしてください
