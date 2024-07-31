3dc0586225d5f49465a2aeb10cd7329e516bdf7b
kameyama <eri.kameyama@giztech-pro.com>
2024-07-31 17:57:30 +0900

    refactor: useEffectを利用したAPI通信回数の削減

--- src/components/CharacterImage/index.jsx
+++ src/components/CharacterImage/index.jsx
@@ -1,13 +1,15 @@
 import axios from 'axios'
+import { useEffect } from 'react'
 import { useState } from 'react'
 
 export const CharacterImage = () => {
   const [imgUrl, setImgUrl] = useState('')
-
-  axios.get('https://pokeapi.co/api/v2/pokemon/1').then((res) => {
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/1').then((res) => {
+      console.log(res.data.sprites.other['official-artwork']['front_default'])
     setImgUrl(res.data.sprites.other['official-artwork']['front_default'])
   })
-
+  }, [])
   return (
     <>
       {imgUrl && <img src={imgUrl} width={100} height={100} />}
