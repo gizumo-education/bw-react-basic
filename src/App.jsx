4a5217e11215ddf86f6888266e9e4bd1abe3f369
sushishinichi1 <sushishinichi3@gmail.com>
2024-08-09 15:00:02 +0900

    add:コンポーネントの課題

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,14 @@
 import reactLogo from './assets/react.svg'
+import React from 'react';
 import styles from './App.module.css'
+
+
+export const App = () => {
+  return (
+    <div>
+      <img src={reactLogo} className={styles['react-logo']}/>
+      <h1>Welcome React!</h1>
+      <p>React Lessonへようこそ</p>
+    </div>
+  )
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import { App } from './App.jsx'
+import { App }  from './App.jsx'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
