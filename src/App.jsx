9c0fb8cf0aee2eb694e3efed1f3e122837fe2ee0
sushishinichi1 <sushishinichi3@gmail.com>
2024-08-09 15:17:26 +0900

    add:コンポーネントのインポートとエクスポート

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import reactLogo from './assets/react.svg'
 import React from 'react';
 import styles from './App.module.css'
+import Button from './components/Button'
 
 
 export const App = () => {
@@ -9,6 +10,10 @@ export const App = () => {
       <img src={reactLogo} className={styles['react-logo']}/>
       <h1>Welcome React!</h1>
       <p>React Lessonへようこそ</p>
+      <hr />
+      <Button />
+      <Button />
+      <Button />
     </div>
   )
 };
