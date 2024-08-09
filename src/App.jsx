630e390a621662190c3770c0ee958026b207452b
sushishinichi1 <sushishinichi3@gmail.com>
2024-08-09 15:46:57 +0900

    add: jsx構文

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import reactLogo from './assets/react.svg'
 import React from 'react';
 import styles from './App.module.css'
-import Button from './components/Button'
+import { Profile } from './components/Profile'
 
 
 export const App = () => {
   return (
     <div>
       <img src={reactLogo} className={styles['react-logo']}/>
-      <h1>Welcome React!</h1>
       <p>React Lessonへようこそ</p>
       <hr />
-      <Button />
-      <Button />
-      <Button />
+      <Profile />
     </div>
   )
 };
