f3f506dd9f86a49b89a8dd6c2f5829ce2cdea94a
mihonakagawa <ohim212welldone@gmail.com>
2024-08-13 23:56:51 +0900

    update: use the useState hook in InputField component

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,11 +1,20 @@
 import styles from './index.module.css'
+import { useState } from 'react'
 
 export const InputField = () => {
-  let value = ''
+  let [value, setValue] = useState('')
 
+  const handleChange = (e) => {
+    // setValue(value + e.target.value) ← これだと入力のたびに入力欄内の全ての値が追加されてしまう（i++と同様の動き）
+    setValue(e.target.value)
+  }
+  
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
+      {/* 元々の記述↓ */}
+      {/* <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' /> */}
+      
+      <input onChange={handleChange} className={styles.input} placeholder='任意の文字を入力' />
       <p>入力文字：{value}</p>
     </>
   )
