e8facd4215ff12e77aa69627916196e312ad35e4
若林 汐音 <wakabayashishione@MacBook-Air-3.local>
2024-09-05 20:29:09 +0900

    add:セクション４〜１０の実装

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,12 +1,13 @@
 import styles from './index.module.css'
+import { useState } from 'react'
 
 export const InputField = () => {
-  let value = ''
-
+  const [value, setValue] = useState()
+  
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
-      <p>入力文字：{value}</p>
+      <input onChange={(e) => setValue(e.target.value)} className={styles.input} placeholder='任意の文字を入力' />
+      <p>useStateを完全に理解した：{value}</p>
     </>
   )
 }
