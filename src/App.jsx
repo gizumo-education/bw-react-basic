bdb0a90317b01b076cc21b5317dd04d794d8cc5e
kanakoAzm <knka0717@gmail.com>
2024-09-25 06:21:33 +0900

    React Lesson Section8 リストレンダー 練習問題の解答

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Profile } from './components/Profile'
 import { Country } from './components/Country'
 import { Card } from './components/Card'
 import { TodoItem } from './components/TodoItem'
+import { LanguageList } from './components/LanguageList'
 
 export const App = () => {
   return (
@@ -39,6 +40,10 @@ export const App = () => {
           <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
           <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
         </ul>
+
+      <hr />
+      {/* -- LanguageListコンポーネントのタグをここに記述 -- */}
+      <LanguageList />
 
     </div>
   );
