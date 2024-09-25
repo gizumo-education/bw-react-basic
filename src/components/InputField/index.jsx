faf9eab33fcebb414b03cf5b03e73263a5252313
kanakoAzm <knka0717@gmail.com>
2024-09-25 11:51:47 +0900

    React Lesson Section10 フックとuseState 練習問題の解答

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,11 +1,14 @@
 import styles from './index.module.css'
+import { useState } from 'react'
 
 export const InputField = () => {
-  let value = ''
-
+  const [value, setValue] = useState('')
+  const handleChange = (e) => {
+    setValue(e.target.value)  // (e) => value = e.target.value
+  }
   return (
     <>
-      <input onChange={(e) => value = e.target.value} className={styles.input} placeholder='任意の文字を入力' />
+      <input className={styles.input} placeholder='任意の文字を入力' onChange={handleChange} />
       <p>入力文字：{value}</p>
     </>
   )
