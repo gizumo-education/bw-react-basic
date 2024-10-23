edefcfc4ee0156090b8672bcafff86635e264071
SakiyamaShun <sakiyama.gizumo@gmail.com>
2024-10-23 16:43:52 +0900

    update 修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
+import {Button} from './components/Button'
 
-// 以下に練習問題の回答を記述してください
 export const App = () => {
   return (
     <div>
       <img src={reactLogo} className={styles['react-logo']} />
       <h1>Hello React!</h1>
-
+      <hr />
       {/* 以下に練習問題の回答を記述してください */}
+      <Button />
+      <Button />
+      <Button />
     </div>
 
   )
 }
+
