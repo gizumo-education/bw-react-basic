56c1f42301fe5da92001a84c6759e4f304ee9205
kiyosakitaisei <kiyotai06@gmail.com>
2024-10-24 15:09:02 +0900

    docs:Section4練習問題セルフチェック修正

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -21,17 +21,16 @@ import styles from './index.module.css'
 
 export const Profile = () => {
   const imgUrl = 'https://i.imgur.com/7vQD0fPs.jpg'
-  const getBirthDay = () => '2023/7/7'
+  const name = 'John'
+  const getBirthDay = () => new Date().toLocaleDateString()
 
   return (
     <>
       <img
         src={imgUrl}
         className={styles.avatar}
-        name = 'John'
-    
       />
-      <p className={styles.sentence}>I am John</p>
+      <p className={styles.sentence}>I am {name}</p>
       <p className={styles.sentence}>BirthDay:{getBirthDay()} </p>
     </>
   )
