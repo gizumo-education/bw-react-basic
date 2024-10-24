d397d6870a647a64482f4bf634b5b606cd4080e7
SakiyamaShun <sakiyama.gizumo@gmail.com>
2024-10-24 12:28:16 +0900

    update Section06完了

--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,10 +1,27 @@
 import styles from './index.module.css'
+import PropTypes from 'prop-types'
 
-export const Country = () => {
+export const Country = ({info, width, height}) => {
   return (
     <div>
-      <img src={info.imgUrl} width={width} height={height} />
+      <img 
+      src={info.imgUrl} 
+      alt={info.name}
+      width={width} 
+      height={height} 
+      />
       <p className={styles.name}>{info.name}</p>
     </div>
   )
+}
+
+Country.prototype = {
+  info: PropTypes.object,
+  width:PropTypes.number,
+  height:PropTypes.number
+}
+
+Country.defaultProps = {
+  width: 120,
+  height: 80
 }
