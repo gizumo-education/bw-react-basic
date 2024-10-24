ae77512353fba720b3d3ebe58693d78adb306580
kiyosakitaisei <kiyotai06@gmail.com>
2024-10-24 14:48:43 +0900

    docs:Section5練習問題

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import { Button } from './components/Button'
+import { Profile } from './components/Profile'
 
 export const App = () => {
   return (
@@ -9,12 +10,19 @@ export const App = () => {
     <h1>Hello React!</h1>
 
     {/* 以下に練習問題の回答を記述してください */}
+
     <hr />
+    
     {/* Section4 */}
     <Button />
     <Button />
     <Button />
 
+    <hr />
+
+    {/* Section5 */}
+    <Profile />
+    
     <hr />
 
 
--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,15 +1,38 @@
+// import styles from './index.module.css'
+
+// export const Profile = () => {
+//   const imgUrl = 'https://i.imgur.com/7vQD0fPs.jpg';
+//   const name = 'John'
+//   const getBirthDay = () => {
+//     return new Date().toLocaleDateString()
+//   }
+
+//   return (
+//     <img src='imgUrl' className={styles.avatar}>
+//     <p className={styles.sentence}>I am John</p>
+//     <p className={styles.sentence}>BirthDay: </p>
+//   )
+// }
+
+
+
+
 import styles from './index.module.css'
 
 export const Profile = () => {
-  const imgUrl = 'https://i.imgur.com/7vQD0fPs.jpg';
-  const name = 'John'
-  const getBirthDay = () => {
-    return new Date().toLocaleDateString()
-  }
+  const imgUrl = 'https://i.imgur.com/7vQD0fPs.jpg'
+  const getBirthDay = () => '2023/7/7'
 
   return (
-    <img src='imgUrl' className={styles.avatar}>
-    <p className={styles.sentence}>I am John</p>
-    <p className={styles.sentence}>BirthDay: </p>
+    <>
+      <img
+        src={imgUrl}
+        className={styles.avatar}
+        name = 'John'
+    
+      />
+      <p className={styles.sentence}>I am John</p>
+      <p className={styles.sentence}>BirthDay:{getBirthDay()} </p>
+    </>
   )
 }
