ffcaba88375f69a67425ef183f65f33b7a1875c7
kiyosakitaisei <kiyotai06@gmail.com>
2024-10-28 00:51:31 +0900

    docs:Section9練習問題

--- src/components/AlertButton/index.jsx
+++ src/components/AlertButton/index.jsx
@@ -1,5 +1,20 @@
+// import styles from './index.module.css'
+
+// export const AlertButton = () => {
+//   return <button className={styles.button}>Alert</button>
+// }
+
+
+
+
+
+
 import styles from './index.module.css'
 
 export const AlertButton = () => {
-  return <button className={styles.button}>Alert</button>
+  const handleAlert = () => {
+    alert('ああああああああ')
+  }
+
+  return <button className={styles.button} onClick={handleAlert}>Alert</button>
 }
