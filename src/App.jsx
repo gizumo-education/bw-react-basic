6af243ddfca52f42d21e04f7bf48cd4a873eeee6
TOMOYA <jingcaoq@gmail.com>
2024-11-27 12:01:24 +0900

    section5の練習問題が終わりJohnのデータを載せました

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import { Button } from './components/Button/index.jsx'
+import { Profile } from './components/Profile/index.jsx'
+
 
 export const App = () => {
   return (
-    <div>
+    <>
       <img src={reactLogo} className={styles['react-logo']} />
       <h1>Hello React!</h1>
       {/* 以下に練習問題の回答を記述してください */}
@@ -12,6 +14,9 @@ export const App = () => {
       <Button />
       <Button />
       <Button />
-    </div>
+
+      <hr />
+      {Profile()}
+    </>
   )
 }
