bf3634d19cfa2593d0c358d4fb78daac473e106e
TOMOYA <jingcaoq@gmail.com>
2024-11-27 17:58:17 +0900

    section7の練習問題が終わりました。

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,7 +2,16 @@ import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
 export const TodoItem = ({ name, isToday, isCompleted }) => {
-  return <li className={styles.name}>{name}</li>
+  return (
+    <>
+      <ul>
+        {/* 左に参考にするものをおき、左ががtrueの時に右が実行する */}
+        {isToday && <li className={styles.name}>{name}{ isCompleted && '✔' }</li>}
+        {/* <li className={styles.name}>{name}{isCompleted && '✔'}</li> */}
+
+      </ul>
+    </>
+  )
 }
 
 TodoItem.propTypes = {
