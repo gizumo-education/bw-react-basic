10e824e860ad17199edce7f47aacafc5f7a25627
村上　諒 <murakamiryo@murakamiryounoMacBook-Pro.local>
2024-12-06 23:21:53 +0900

    section7の練習問題記入

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Button from './components/Button'
 import {Profile} from './components/Profile/index.jsx'
 import {Props} from './components/Country/index.jsx'
 import { Card } from './components/Card/index.jsx'
+import {TodoItem} from './components/TodoItem/index.jsx'
 
 export const App = () => {
   return(
@@ -16,12 +17,18 @@ export const App = () => {
       <Button></Button>
       <Button></Button>
       <Button></Button>
-      <hr />
-    <Profile></Profile>
     <hr />
-    <Card>
-    <Props></Props>
+      <Profile></Profile>
+    <hr />
+      <Card>
+      <Props></Props>
     </Card>
+    <hr />
+    <ul>
+      <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+      <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+      <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+    </ul>
       </div>
  
   )
--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,12 +1,28 @@
 import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
+// export const TodoItem = ({ name, isToday, isCompleted }) => {
+//   return <li className={styles.name}>{name}</li>
+// }
+// 
+
 export const TodoItem = ({ name, isToday, isCompleted }) => {
-  return <li className={styles.name}>{name}</li>
-}
+  // 今日実施するToDoのみ表示
+  if (!isToday) {
+    return null;
+  }
+
+  return (
+    <li className={styles.name}>
+      {name}
+      {isCompleted && " ✔︎"}
+    </li>
+  );
+};
 
 TodoItem.propTypes = {
   name: PropTypes.string.isRequired,
   isToday: PropTypes.bool.isRequired,
   isCompleted: PropTypes.bool.isRequired,
 }
+
