0b839a2ae9d4a96898c337fd6e0a817b837def26
村上　諒 <murakamiryo@murakamiryounoMacBook-Pro.local>
2024-12-08 21:59:04 +0900

    section10の練習問題記入

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Card } from './components/Card/index.jsx'
 import {TodoItem} from './components/TodoItem/index.jsx'
 import {LanguageList} from './components/LanguageList/index.jsx'
 import {AlertButton} from './components/AlertButton/index.jsx'
+import {InputField} from './components/InputField/index.jsx'
 
 export const App = () => {
   return(
@@ -35,9 +36,11 @@ export const App = () => {
     <LanguageList></LanguageList>
     <hr />
     <AlertButton></AlertButton>
-
+    <hr />
+    <InputField></InputField>
+    <hr />
   </div>
-  ) 
+  )
 };
 
   
