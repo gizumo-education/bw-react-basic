36c497dd5d47352c666e5b83d763891511d7601e
村上　諒 <murakamiryo@murakamiryounoMacBook-Pro.local>
2024-12-17 12:55:37 +0900

    section8の問題記入

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,41 @@ import { Profile } from './components/Profile/index.jsx'
 import { Country } from './components/Country/index.jsx'
 import { Card } from './components/Card/index.jsx'
 import { TodoItem } from './components/TodoItem/index.jsx'
+import { LanguageList } from './components/LanguageList/index.jsx'
 
 export const App = () => {
   return(
-    <div>
-      <img src={reactLogo} className={styles['react-logo']} />
-      <h1>Hello React!</h1>
+  <div>
+    <img src={reactLogo} className={styles['react-logo']} />
+    <h1>Hello React!</h1>
 
       {/* 以下に練習問題の回答を記述してください */}
-      <hr />
+  <hr />
       <Button></Button>
       <Button></Button>
       <Button></Button>
-      <hr />
+  <hr />
       <Profile></Profile>
-      <hr />
-<Card>
-<Country
-width={120}
-height={80}
-info = {{
-        imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
-        name: 'アメリカ'
-      }}
-/>
-</Card>
-<hr />
-  <ul>
-      <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
-      <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
-      <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
-  </ul>
+  <hr />
+      <Card>
+      <Country
+        width={120}
+        height={80}
+        info = {{
+                imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+                name: 'アメリカ'
+              }}
+      />
+      </Card>
+  <hr />
+      <ul>
+        <TodoItem name='Reactの学習' isToday={true} isCompleted={true} />
+        <TodoItem name='HTMLの復習' isToday={true} isCompleted={false} />
+        <TodoItem name='React Hooksの予習' isToday={false} isCompleted={false} />
+      </ul>
+  <hr/>
+    <LanguageList></LanguageList>
+  <hr/>
     </div>
   )
 };
