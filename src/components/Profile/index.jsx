c016b6bf6b7da940421b72024386f563731edfb3
村上　諒 <murakamiryo@murakamiryounoMacBook-Pro.local>
2024-12-17 14:36:04 +0900

    section5、index.jsx内の定数記述の修正

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -17,13 +17,12 @@ import styles from './index.module.css'
 export const Profile = () => {
   const imgUrl = 'https://i.imgur.com/7vQD0fPs.jpg';
   const name = 'John'
-  const getBirthDay = () => {
-    return new Date().toLocaleDateString()
-  }
+  const getBirthDay = () => '2023/7/7'
 
 return (<>
   <img src={ imgUrl } className={styles.avatar}></img>
-  <p className={styles.sentence}>I am John</p>
-  <p className={styles.sentence}>BirthDay:2023/7/7</p>
-</>)
+  <p className={styles.sentence}>I am{name}</p>
+      <p className={styles.sentence}>BirthDay:{getBirthDay()}</p>
+    </>
+  )
 };
